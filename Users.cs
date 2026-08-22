b01705059537975ca84ff74fdf105ad9029f264c
Aladdin Anwar <32776609+zataaa@users.noreply.github.com>
2026-08-22 22:24:14 +0300

    Update Users.cs

--- Users.cs
+++ Users.cs
@@ -1,4 +1,6 @@
+
 #nullable enable
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -9,23 +11,33 @@ namespace WebApplication1.Models
     public class Users
     {
         [Key]
-        [Column("user_id")]
+        [Column("UserID")]
         public int user_id { get; set; }
 
         [Required]
         [MaxLength(200)]
-        [Column("user_name")]
+        [Column("UserName")]
         public string user_name { get; set; } = "";
 
         [Required]
         [MaxLength(400)]
-        [Column("email")]
+        [Column("Email")]
         public string email { get; set; } = "";
 
         [Required]
         [Column("CreatedAT")]
         public DateTime CreatedAT { get; set; }
 
+        [Required]
+        [Column("PasswordHash")]
+        public string PasswordHash { get; set; } = "";
 
+        [Required]
+        [Column("Status")]
+        public string Status { get; set; } = "";
+
+        [Column("Notes")]
+        public string Notes { get; set; } = "";
+        
     }
 }
