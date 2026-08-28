6f6fdf94a505fc8740118070c5a6cb325d922b40
Aladdin Anwar <32776609+zataaa@users.noreply.github.com>
2026-08-28 19:18:06 +0300

    Add files via upload

---
+++ Steps for safe deletion.sql
@@ -0,0 +1,34 @@
+﻿
+-- حذف Procedure من الاتي
+-- =============================================
+-- Stored Procedure: [dbo].[GetAllEntitiesLedgerSummaryPivot]
+-- =============================================
+
+-- =============================================
+-- Stored Procedure: [dbo].[GetAllEntitiesLedgerSummary]
+-- =============================================
+
+-- =============================================
+-- Stored Procedure: [dbo].[GetAllMerchantsFinancialSummary]
+-- =============================================
+
+-- =============================================
+-- Stored Procedure: [dbo].[GetFinancialSummary]
+-- =============================================
+
+-- =============================================
+-- Stored Procedure: [dbo].[GetMerchantFinancialSummary]
+-- =============================================
+
+   DROP PROCEDURE GetAllEntitiesLedgerSummaryPivot;
+   DROP PROCEDURE GetAllMerchantsFinancialSummary;
+   DROP PROCEDURE GetFinancialSummary;
+   DROP PROCEDURE GetMerchantFinancialSummary;
+   DROP PROCEDURE AddBigMerchant;
+   DROP PROCEDURE AddSmallMerchant;
+
+ALTER TABLE SmallMerchant DROP CONSTRAINT PK__SmallMer__0B135F59C3486A43;
+ALTER TABLE BigMerchant DROP CONSTRAINT PK__BigMerch__991621073AC92CD6;
+
+DROP TABLE SmallMerchant;
+DROP TABLE BigMerchant;
